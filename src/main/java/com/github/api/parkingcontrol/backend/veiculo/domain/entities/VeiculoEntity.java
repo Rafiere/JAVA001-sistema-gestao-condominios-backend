61dfb26a3065046a0e4007a34462cd79fb0e18c5
Rafael <rafapgbr@gmail.com>
2022-07-30 20:59:55 -0300

    feat: inserção de entidade para veículo.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/veiculo/domain/entities/VeiculoEntity.java
@@ -0,0 +1,31 @@
+package com.github.api.parkingcontrol.backend.veiculo.domain.entities;
+
+import com.github.api.parkingcontrol.backend.estacionamento.domain.VagaEstacionamento;
+import com.github.api.parkingcontrol.backend.morador.domain.entities.MoradorEntity;
+import com.github.api.parkingcontrol.backend.veiculo.domain.Veiculo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class VeiculoEntity {
+
+    @Id
+    private final String id = UUID.randomUUID().toString();
+
+    private String placaDoVeiculo;
+
+    private String marcaDoVeiculo;
+
+    private String modeloDoVeiculo;
+
+    private String corDoVeiculo;
+}
