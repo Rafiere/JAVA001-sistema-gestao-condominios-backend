e2559b2ab19effdc44ec73051f4cfdb6170ab364
Rafael <rafapgbr@gmail.com>
2022-07-30 20:20:58 -0300

    feat: adição de porta para verificar se existe algum morador no apartamento.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/apartamento/application/port/out/VerificarSeExisteMoradorNoApartamentoPort.java
@@ -0,0 +1,6 @@
+package com.github.api.parkingcontrol.backend.apartamento.application.port.out;
+
+public interface VerificarSeExisteMoradorNoApartamentoPort {
+
+    boolean verificarSeExisteMoradorNoApartamento(String idApartamento);
+}
