c97e3944a2ca2c807530487e4fde8b930107d5d0
Rafael <rafapgbr@gmail.com>
2022-07-30 20:20:12 -0300

    feat: adição de command para remover um apartamento.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/apartamento/adapter/in/web/commands/RemoverApartamentoCommand.java
@@ -0,0 +1,9 @@
+package com.github.api.parkingcontrol.backend.apartamento.adapter.in.web.commands;
+
+import lombok.Value;
+
+@Value
+public class RemoverApartamentoCommand {
+
+    String idApartamento;
+}
