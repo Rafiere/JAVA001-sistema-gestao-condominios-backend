17df20f53c1419ce6feff34e4b0deabef3c4e46b
Rafael <rafapgbr@gmail.com>
2022-07-30 20:20:19 -0300

    feat: adição de controller para remover um apartamento.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/apartamento/adapter/in/web/RemoverApartamentoController.java
@@ -0,0 +1,26 @@
+package com.github.api.parkingcontrol.backend.apartamento.adapter.in.web;
+
+import com.github.api.parkingcontrol.backend.apartamento.adapter.in.web.commands.RemoverApartamentoCommand;
+import com.github.api.parkingcontrol.backend.apartamento.application.RemoverApartamentoUseCase;
+import com.github.api.parkingcontrol.backend.apartamento.domain.Apartamento;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/apartamento")
+@RequiredArgsConstructor
+public class RemoverApartamentoController {
+
+    private final RemoverApartamentoUseCase removerApartamentoUseCase;
+
+    @DeleteMapping("/remover-apartamento")
+    public ResponseEntity<Apartamento> execute(@RequestParam String idApartamento) {
+
+        RemoverApartamentoCommand command = new RemoverApartamentoCommand(idApartamento);
+
+        removerApartamentoUseCase.execute(command);
+
+        return ResponseEntity.status(204).body(null);
+    }
+}
