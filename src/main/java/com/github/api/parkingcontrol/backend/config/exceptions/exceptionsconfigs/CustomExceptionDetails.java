ed185f2c62c0a3c5fcd7d6651b8d4a08785f0a0f
Rafael <rafapgbr@gmail.com>
2022-07-30 12:52:57 -0300

    feat: criação de detalhes customizados para as exceptions.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/config/exceptions/exceptionsconfigs/CustomExceptionDetails.java
@@ -0,0 +1,8 @@
+package com.github.api.parkingcontrol.backend.config.exceptions.exceptionsconfigs;
+
+import com.github.api.parkingcontrol.backend.config.exceptions.exceptionsconfigs.ExceptionDetails;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+public class CustomExceptionDetails extends ExceptionDetails {
+}
