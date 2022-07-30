452ad143eaee79d1f920e39fc1fa4fb7ee3b019e
Rafael <rafapgbr@gmail.com>
2022-07-30 20:17:51 -0300

    feat: adição de repository para o apartamento.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/apartamento/adapter/out/persistence/ApartamentoRepository.java
@@ -0,0 +1,17 @@
+package com.github.api.parkingcontrol.backend.apartamento.adapter.out.persistence;
+
+import com.github.api.parkingcontrol.backend.apartamento.domain.entities.ApartamentoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApartamentoRepository extends JpaRepository<ApartamentoEntity, String> {
+
+    @Query("SELECT COUNT(m) " +
+            "FROM MoradorEntity m " +
+            "LEFT JOIN m.apartamentoEntities ap " +
+            "WHERE ap.id =: idApartamento ")
+    boolean verificarSeExisteMoradorNoApartamento(String idApartamento);
+}
