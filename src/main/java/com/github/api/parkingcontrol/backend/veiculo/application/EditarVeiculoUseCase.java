018ace3cd1cf29c4ff669979617ca9290d2a02a7
Rafael <rafapgbr@gmail.com>
2022-07-31 13:45:35 -0300

    feat: inserção de usecase para editar veículo.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/veiculo/application/EditarVeiculoUseCase.java
@@ -0,0 +1,28 @@
+package com.github.api.parkingcontrol.backend.veiculo.application;
+
+import com.github.api.parkingcontrol.backend.veiculo.adapter.in.web.commands.EditarVeiculoCommand;
+import com.github.api.parkingcontrol.backend.veiculo.application.port.out.BuscarVeiculoPeloIdPort;
+import com.github.api.parkingcontrol.backend.veiculo.application.port.out.CadastrarVeiculoPort;
+import com.github.api.parkingcontrol.backend.veiculo.domain.Veiculo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EditarVeiculoUseCase {
+
+    private final CadastrarVeiculoPort cadastrarVeiculoPort;
+    private final BuscarVeiculoPeloIdPort buscarVeiculoPeloIdPort;
+
+    public Veiculo execute(EditarVeiculoCommand command) {
+
+        Veiculo veiculo = buscarVeiculoPeloIdPort.buscarVeiculoPeloId(command.getId());
+
+        veiculo.atualizar(command.getPlacaDoVeiculo(),
+                command.getMarcaDoVeiculo(),
+                command.getModeloDoVeiculo(),
+                command.getCorDoVeiculo());
+
+        return cadastrarVeiculoPort.cadastrarVeiculo(veiculo);
+    }
+}
