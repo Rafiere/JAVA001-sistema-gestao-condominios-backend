26ba426825cbc211e84759a356a868cf1d48cecc
Rafael <rafapgbr@gmail.com>
2022-07-31 13:44:32 -0300

    feat: inserção de usecase para remover vaga de estacionamento.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/estacionamento/application/RemoverVagaEstacionamentoUseCase.java
@@ -0,0 +1,18 @@
+package com.github.api.parkingcontrol.backend.estacionamento.application;
+
+import com.github.api.parkingcontrol.backend.estacionamento.adapter.in.web.command.RemoverVagaEstacionamentoCommand;
+import com.github.api.parkingcontrol.backend.estacionamento.application.port.out.RemoverVagaEstacionamentoPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RemoverVagaEstacionamentoUseCase {
+
+    private final RemoverVagaEstacionamentoPort removerVagaEstacionamentoPort;
+
+    public void execute(RemoverVagaEstacionamentoCommand command) {
+
+        removerVagaEstacionamentoPort.removerVagaEstacionamento(command.getIdVagaEstacionamento());
+    }
+}
