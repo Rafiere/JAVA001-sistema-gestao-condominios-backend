cef553f708cee2ce2ad0fb7e2e4afd9c3d98c62a
Rafael <rafapgbr@gmail.com>
2022-07-31 13:44:38 -0300

    feat: inserção de port para remover vaga de estacionamento.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/estacionamento/application/port/out/RemoverVagaEstacionamentoPort.java
@@ -0,0 +1,6 @@
+package com.github.api.parkingcontrol.backend.estacionamento.application.port.out;
+
+public interface RemoverVagaEstacionamentoPort {
+
+    void removerVagaEstacionamento(String idVagaEstacionamento);
+}
