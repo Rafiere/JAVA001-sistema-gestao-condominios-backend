f3c69d4bc3b8800d8d60f35472442ea552f42c49
Rafael <rafapgbr@gmail.com>
2022-07-30 21:00:17 -0300

    feat: inserção de entidade para morador.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/morador/domain/entities/MoradorEntity.java
@@ -0,0 +1,41 @@
+package com.github.api.parkingcontrol.backend.morador.domain.entities;
+
+import com.github.api.parkingcontrol.backend.apartamento.domain.Apartamento;
+import com.github.api.parkingcontrol.backend.apartamento.domain.entities.ApartamentoEntity;
+import com.github.api.parkingcontrol.backend.estacionamento.domain.VagaEstacionamento;
+import com.github.api.parkingcontrol.backend.estacionamento.domain.entities.VagaEstacionamentoEntity;
+import com.github.api.parkingcontrol.backend.morador.domain.Morador;
+import com.github.api.parkingcontrol.backend.veiculo.domain.Veiculo;
+import com.github.api.parkingcontrol.backend.veiculo.domain.entities.VeiculoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class MoradorEntity {
+
+    @Id
+    private final String id = UUID.randomUUID().toString();
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String cpf;
+
+    @OneToMany
+    private List<ApartamentoEntity> apartamento;
+
+    @OneToMany
+    private List<VeiculoEntity> veiculos;
+
+    @OneToMany
+    private List<VagaEstacionamentoEntity> vagasEstacionamento;
+}
