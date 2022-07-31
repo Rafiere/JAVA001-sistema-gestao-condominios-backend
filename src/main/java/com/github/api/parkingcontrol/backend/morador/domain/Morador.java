f2640a65fe8233b72b7d8bda98318751cee2ef44
Rafael <rafapgbr@gmail.com>
2022-07-31 10:03:50 -0300

    feat: inserção de método construtor personalizado e de método para atualizar um morador.

--- src/main/java/com/github/api/parkingcontrol/backend/morador/domain/Morador.java
+++ src/main/java/com/github/api/parkingcontrol/backend/morador/domain/Morador.java
@@ -4,11 +4,9 @@ import com.github.api.parkingcontrol.backend.apartamento.domain.Apartamento;
 import com.github.api.parkingcontrol.backend.estacionamento.domain.VagaEstacionamento;
 import com.github.api.parkingcontrol.backend.veiculo.domain.Veiculo;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.OneToMany;
 import java.util.List;
 
 @Getter
@@ -24,14 +22,44 @@ public class Morador {
 
     private String cpf;
 
-    private List<Apartamento> apartamento;
+    private List<Apartamento> apartamentos;
 
     private List<Veiculo> veiculos;
 
     private List<VagaEstacionamento> vagasEstacionamento;
 
+
+    public Morador(String nome,
+                   String sobrenome,
+                   String cpf,
+                   List<Apartamento> apartamentos,
+                   List<Veiculo> veiculos,
+                   List<VagaEstacionamento> vagasEstacionamento) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.apartamentos = apartamentos;
+        this.veiculos = veiculos;
+        this.vagasEstacionamento = vagasEstacionamento;
+    }
+
     public String obterNomeCompleto(){
 
         return this.nome + " " + this.sobrenome;
+    }
+
+    public void atualizar(String nome,
+                          String sobrenome,
+                          String cpf,
+                          List<Apartamento> apartamentosDoMorador,
+                          List<Veiculo> veiculosDoMorador,
+                          List<VagaEstacionamento> vagasEstacionamentoDoMorador) {
+
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.apartamentos = apartamentosDoMorador;
+        this.veiculos = veiculosDoMorador;
+        this.vagasEstacionamento = vagasEstacionamentoDoMorador;
     }
 }
