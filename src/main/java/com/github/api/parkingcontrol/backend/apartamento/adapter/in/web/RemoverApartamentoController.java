6c03fa429bee7c3960ac61f1414624c1ef764bbb
Rafael <rafapgbr@gmail.com>
2022-07-31 10:05:59 -0300

    refactor! alteração da response para "void".

--- src/main/java/com/github/api/parkingcontrol/backend/apartamento/adapter/in/web/RemoverApartamentoController.java
+++ src/main/java/com/github/api/parkingcontrol/backend/apartamento/adapter/in/web/RemoverApartamentoController.java
@@ -2,7 +2,6 @@ package com.github.api.parkingcontrol.backend.apartamento.adapter.in.web;
 
 import com.github.api.parkingcontrol.backend.apartamento.adapter.in.web.commands.RemoverApartamentoCommand;
 import com.github.api.parkingcontrol.backend.apartamento.application.RemoverApartamentoUseCase;
-import com.github.api.parkingcontrol.backend.apartamento.domain.Apartamento;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +14,7 @@ public class RemoverApartamentoController {
     private final RemoverApartamentoUseCase removerApartamentoUseCase;
 
     @DeleteMapping("/remover-apartamento")
-    public ResponseEntity<Apartamento> execute(@RequestParam String idApartamento) {
+    public ResponseEntity<Void> execute(@RequestParam String idApartamento) {
 
         RemoverApartamentoCommand command = new RemoverApartamentoCommand(idApartamento);
 
