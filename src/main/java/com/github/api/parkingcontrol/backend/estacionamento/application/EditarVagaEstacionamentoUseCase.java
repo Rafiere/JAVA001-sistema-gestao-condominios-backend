be06480ab4569970047108c3d717e8cd8aebe1c9
Rafael <rafapgbr@gmail.com>
2022-07-31 13:46:50 -0300

    feat: inserção de usecase para editar vaga de estacionamento.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/estacionamento/application/EditarVagaEstacionamentoUseCase.java
@@ -0,0 +1,28 @@
+package com.github.api.parkingcontrol.backend.estacionamento.application;
+
+import com.github.api.parkingcontrol.backend.estacionamento.adapter.in.web.command.EditarVagaEstacionamentoCommand;
+import com.github.api.parkingcontrol.backend.estacionamento.application.port.out.BuscarVagaEstacionamentoPorIdPort;
+import com.github.api.parkingcontrol.backend.estacionamento.application.port.out.CadastrarVagaEstacionamentoPort;
+import com.github.api.parkingcontrol.backend.estacionamento.domain.VagaEstacionamento;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EditarVagaEstacionamentoUseCase {
+
+    private final CadastrarVagaEstacionamentoPort cadastrarVagaEstacionamentoPort;
+    private final BuscarVagaEstacionamentoPorIdPort buscarVagaEstacionamentoPorIdPort;
+
+    public VagaEstacionamento execute(EditarVagaEstacionamentoCommand command) {
+
+        VagaEstacionamento vagaEstacionamento
+                = buscarVagaEstacionamentoPorIdPort.buscarVagaEstacionamentoPorId(command.getIdVagaEstacionamento());
+
+        vagaEstacionamento.atualizar(
+                command.getLinhaEstacionamento(),
+                command.getColunaEstacionamento());
+
+        return cadastrarVagaEstacionamentoPort.cadastrarVagaEstacionamento(vagaEstacionamento);
+    }
+}
