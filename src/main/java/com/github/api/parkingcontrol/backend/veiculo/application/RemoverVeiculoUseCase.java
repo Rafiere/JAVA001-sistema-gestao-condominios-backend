03f3739585ffa67c978e966d7b184677a0cfa87e
Rafael <rafapgbr@gmail.com>
2022-07-31 13:44:20 -0300

    feat: inserção de usecase para remover veículo.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/veiculo/application/RemoverVeiculoUseCase.java
@@ -0,0 +1,18 @@
+package com.github.api.parkingcontrol.backend.veiculo.application;
+
+import com.github.api.parkingcontrol.backend.veiculo.adapter.in.web.commands.RemoverVeiculoCommand;
+import com.github.api.parkingcontrol.backend.veiculo.application.port.out.RemoverVeiculoPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RemoverVeiculoUseCase {
+
+    private final RemoverVeiculoPort removerVeiculoPort;
+
+    public void execute(RemoverVeiculoCommand command) {
+
+        removerVeiculoPort.removerVeiculo(command.getIdVeiculo());
+    }
+}
