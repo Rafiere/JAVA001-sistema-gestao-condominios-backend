cae723457b441ed3b630951e145f72d244febb23
Rafael <rafapgbr@gmail.com>
2022-08-01 10:03:24 -0300

    feat: inserção de mapper para usuário.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/usuario/domain/mappers/UsuarioMapper.java
@@ -0,0 +1,36 @@
+package com.github.api.parkingcontrol.backend.usuario.domain.mappers;
+
+import com.github.api.parkingcontrol.backend.usuario.domain.Usuario;
+import com.github.api.parkingcontrol.backend.usuario.domain.entities.UsuarioEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class UsuarioMapper {
+
+    private final CargoMapper cargoMapper;
+
+    public Usuario fromEntity(UsuarioEntity usuarioEntity) {
+
+        return new Usuario(
+                usuarioEntity.getId(),
+                usuarioEntity.getEmail(),
+                usuarioEntity.getUsername(),
+                usuarioEntity.getPassword(),
+                usuarioEntity.isAtivo(),
+                usuarioEntity.getCargos().stream().map(cargoMapper::fromEntity).collect(Collectors.toList()));
+    }
+
+    public UsuarioEntity fromDomain(Usuario usuario) {
+
+        return new UsuarioEntity(
+                usuario.getEmail(),
+                usuario.getUsername(),
+                usuario.getPassword(),
+                usuario.isAtivo(),
+                usuario.getCargos().stream().map(cargoMapper::fromDomain).collect(Collectors.toList()));
+    }
+}
