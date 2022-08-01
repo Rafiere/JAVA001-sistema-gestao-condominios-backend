e27a9b90ff341488177fb471bb53dc641bd32eaa
Rafael <rafapgbr@gmail.com>
2022-08-01 10:04:11 -0300

    feat: inserção de domain para cargo.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/usuario/domain/Cargo.java
@@ -0,0 +1,23 @@
+package com.github.api.parkingcontrol.backend.usuario.domain;
+
+import com.github.api.parkingcontrol.backend.usuario.domain.enums.TipoDeCargo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cargo implements GrantedAuthority {
+
+    private String id;
+
+    private TipoDeCargo cargo;
+
+    @Override
+    public String getAuthority() {
+
+        return this.cargo.toString();
+    }
+}
