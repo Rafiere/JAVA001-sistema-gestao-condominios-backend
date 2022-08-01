c7bd48206c408c2277c7254610ecc6cd4a45bc7d
Rafael <rafapgbr@gmail.com>
2022-08-01 12:31:27 -0300

    fix: corrigindo permissões do Swagger na configuração antiga.

--- src/main/java/com/github/api/parkingcontrol/backend/config/security/WebSecurityConfig.java
+++ src/main/java/com/github/api/parkingcontrol/backend/config/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.github.api.parkingcontrol.backend.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,10 +33,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**/swagger-ui.html",
-                                        URL_RECUPERACAO_DE_SENHA)
-                .permitAll()
-                .antMatchers("/usuario/cadastrar-usuario").hasRole("ADMIN")
+                .antMatchers("/usuario/cadastrar-usuario").hasRole("ADMINISTRADOR")
+                .antMatchers("/**/swagger-ui/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/**/swagger-resources/**",
+                        "/**/webjars/springfox-swagger-ui/**",
+                        "/**/v2/api-docs/**").permitAll()
                 .anyRequest()
                 .authenticated();
     }
