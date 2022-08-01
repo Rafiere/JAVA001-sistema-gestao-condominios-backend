55d589badf41c9e8cc9c0d023ca7c204a1d3fa77
Rafael <rafapgbr@gmail.com>
2022-08-01 10:01:59 -0300

    feat: adição de classe de configuração para o Spring Security.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/config/security/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.github.api.parkingcontrol.backend.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+/**
+ * Esse é o modo antigo de realizar a configuração do Spring Security.
+ */
+
+@Configuration
+@EnableWebSecurity //Essa anotação serve para desligarmos todas as configurações de segurança padrão do Spring Security, assim, toda a configuração deverá ser feita do início.
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        final String URL_RECUPERACAO_DE_SENHA = "NULL";
+
+        http
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**/swagger-ui.html",
+                                        URL_RECUPERACAO_DE_SENHA)
+                .permitAll()
+                .antMatchers("/usuario/cadastrar-usuario").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(userDetailsServiceImpl) //A autenticação será realizada através desse serviço.
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+}
