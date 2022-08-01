30c7779c631377d327e2db229f098a69d7110461
Rafael <rafapgbr@gmail.com>
2022-08-01 12:31:41 -0300

    fix: corrigindo permissões do Swagger na configuração nova.

--- src/main/java/com/github/api/parkingcontrol/backend/config/security/WebSecurityConfigV2.java
+++ src/main/java/com/github/api/parkingcontrol/backend/config/security/WebSecurityConfigV2.java
@@ -1,7 +1,9 @@
 package com.github.api.parkingcontrol.backend.config.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,25 +17,26 @@ public class WebSecurityConfigV2 {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        final String URL_RECUPERACAO_DE_SENHA = "NULL"; //TODO: Inserir URL para a recuperação de senha.
+        final String URL_RECUPERACAO_DE_SENHA = "NULL";
 
         http
-            .httpBasic()
-            .and()
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/**/swagger-ui", //Não está funcionando.
-                    URL_RECUPERACAO_DE_SENHA)
-            .permitAll()
-            .antMatchers("/usuario/cadastrar-usuario").hasRole("ADMIN");
-//            .anyRequest()
-//            .authenticated();
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/usuario/cadastrar-usuario").hasRole("ADMINISTRADOR")
+                .antMatchers("/**/swagger-ui/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/**/swagger-resources/**",
+                        "/**/webjars/springfox-swagger-ui/**",
+                        "/**/v2/api-docs/**").permitAll()
+                .anyRequest()
+                .authenticated();
 
         return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder();
     }
