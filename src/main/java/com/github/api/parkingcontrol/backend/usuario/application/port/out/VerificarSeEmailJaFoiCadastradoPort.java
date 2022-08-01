2fd3d0ff3777c376ea9ace7456533dfc67999ab5
Rafael <rafapgbr@gmail.com>
2022-08-01 10:05:06 -0300

    feat: inserção de porta para verificar se o email já foi cadastrado.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/usuario/application/port/out/VerificarSeEmailJaFoiCadastradoPort.java
@@ -0,0 +1,6 @@
+package com.github.api.parkingcontrol.backend.usuario.application.port.out;
+
+public interface VerificarSeEmailJaFoiCadastradoPort {
+
+    boolean verificarSeEmailJaFoiCadastrado(String email);
+}
