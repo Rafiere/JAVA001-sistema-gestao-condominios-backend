43ea54ab50891e252b97833da040825fc7956d53
Rafael <rafapgbr@gmail.com>
2022-08-01 10:04:52 -0300

    feat: inserção de service para buscar um usuário pelo nome do usuário, requerido pelo Spring Security.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.github.api.parkingcontrol.backend.config.security;
+
+import com.github.api.parkingcontrol.backend.config.exceptions.BusinessRuleException;
+import com.github.api.parkingcontrol.backend.usuario.application.port.out.BuscarUsuarioPeloUsernamePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional //Essa anotação foi utilizada para não tomar um NPE.
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final BuscarUsuarioPeloUsernamePort buscarUsuarioPeloUsernamePort;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserDetails usuarioBuscado = buscarUsuarioPeloUsernamePort.buscarUsuarioPeloUsername(username);
+
+        if(!usuarioBuscado.isEnabled()){
+
+            throw new BusinessRuleException("Esse usuário não está ativo. Por favor, entre em contato com a " +
+                    "administração do sistema ou realize o login com outro usuário.");
+        }
+
+        return usuarioBuscado;
+    }
+}
