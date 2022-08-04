7b1b70a8bb1386a6eb37cc3ce6d08de6e4be8467
Rafael <rafapgbr@gmail.com>
2022-08-04 08:58:07 -0300

    feat: inserção de dependência do Maven para retirar erro ao executar o comando "mvn clean install".

--- src/main/java/com/github/api/parkingcontrol/backend/usuario/application/CadastrarUsuarioUseCase.java
+++ src/main/java/com/github/api/parkingcontrol/backend/usuario/application/CadastrarUsuarioUseCase.java
@@ -1,21 +1,37 @@
 package com.github.api.parkingcontrol.backend.usuario.application;
 
 import com.github.api.parkingcontrol.backend.config.exceptions.BusinessRuleException;
+import com.github.api.parkingcontrol.backend.token.application.EnviarEmailConfirmacaoCadastroUseCase;
+import com.github.api.parkingcontrol.backend.token.application.GerarTokenEmailHelper;
+import com.github.api.parkingcontrol.backend.token.application.port.out.SalvarTokenEmailPort;
+import com.github.api.parkingcontrol.backend.token.domain.TokenEmail;
 import com.github.api.parkingcontrol.backend.usuario.adapter.in.web.command.CadastrarUsuarioCommand;
+import com.github.api.parkingcontrol.backend.usuario.application.port.out.BuscarCargosPorIdsPort;
 import com.github.api.parkingcontrol.backend.usuario.application.port.out.CadastrarUsuarioPort;
 import com.github.api.parkingcontrol.backend.usuario.application.port.out.VerificarSeEmailJaFoiCadastradoPort;
 import com.github.api.parkingcontrol.backend.usuario.application.port.out.VerificarSeUsernameJaFoiCadastradoPort;
+import com.github.api.parkingcontrol.backend.usuario.domain.Cargo;
 import com.github.api.parkingcontrol.backend.usuario.domain.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CadastrarUsuarioUseCase {
 
     private final CadastrarUsuarioPort cadastrarUsuarioPort;
     private final VerificarSeEmailJaFoiCadastradoPort verificarSeEmailJaFoiCadastradoPort;
     private final VerificarSeUsernameJaFoiCadastradoPort verificarSeUsernameJaFoiCadastradoPort;
+    private final SalvarTokenEmailPort salvarTokenEmailPort;
+    private final GerarTokenEmailHelper gerarTokenEmailHelper;
+    private final BuscarCargosPorIdsPort buscarCargosPorIdsPort;
+
+
+    private final EnviarEmailConfirmacaoCadastroUseCase enviarEmailConfirmacaoCadastroUseCase;
 
     public Usuario execute(CadastrarUsuarioCommand command) {
 
@@ -29,14 +45,21 @@ public class CadastrarUsuarioUseCase {
             throw new BusinessRuleException("Esse username já está em uso. Por favor, utilize um outro username.");
         }
 
+        List<Cargo> cargosBuscados =
+                buscarCargosPorIdsPort.buscarCargosPorIds(command.getIdsCargosDoUsuario());
+
         Usuario usuario = new Usuario(
                 command.getEmail(),
                 command.getPassword(),
                 command.getUsername(),
                 command.isAtivo(),
-                command.getCargos());
+                cargosBuscados);
 
-        return cadastrarUsuarioPort.cadastrarUsuario(usuario);
+        Usuario usuarioCriado = cadastrarUsuarioPort.cadastrarUsuario(usuario);
+
+        enviarEmailDeConfirmacaoDeCadastro(usuarioCriado);
+
+        return usuarioCriado;
     }
 
     private boolean usernameJaFoiCadastrado(String username) {
@@ -47,5 +70,16 @@ public class CadastrarUsuarioUseCase {
     private boolean emailJaFoiCadastrado(String email) {
 
         return verificarSeEmailJaFoiCadastradoPort.verificarSeEmailJaFoiCadastrado(email);
+    }
+
+    private void enviarEmailDeConfirmacaoDeCadastro(Usuario usuario){
+
+        TokenEmail tokenEmail = gerarTokenEmailHelper.gerarToken();
+
+        tokenEmail.definirUsuario(usuario);
+
+        TokenEmail tokenEmailSalvo = salvarTokenEmailPort.salvarTokenEmail(tokenEmail);
+
+        enviarEmailConfirmacaoCadastroUseCase.execute(tokenEmailSalvo);
     }
 }
