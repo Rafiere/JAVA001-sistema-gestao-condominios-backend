5ba35fc2a1302b65bc4ed7fcaa123013ff7bdc43
Rafael <rafapgbr@gmail.com>
2022-08-06 19:39:14 -0300

    feat: inserção de método para transformar o domain na entidade com um ID predefinido, sem gerar um novo UUID.

--- src/main/java/com/github/api/parkingcontrol/backend/estacionamento/domain/mappers/VagaEstacionamentoMapper.java
+++ src/main/java/com/github/api/parkingcontrol/backend/estacionamento/domain/mappers/VagaEstacionamentoMapper.java
@@ -4,10 +4,12 @@ import com.github.api.parkingcontrol.backend.estacionamento.domain.VagaEstaciona
 import com.github.api.parkingcontrol.backend.estacionamento.domain.entities.VagaEstacionamentoEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class VagaEstacionamentoMapper {
 
-    public VagaEstacionamento fromEntity(VagaEstacionamentoEntity vagaEstacionamentoEntity){
+    public VagaEstacionamento fromEntity(VagaEstacionamentoEntity vagaEstacionamentoEntity) {
 
         return new VagaEstacionamento(
                 vagaEstacionamentoEntity.getId(),
@@ -15,11 +17,19 @@ public class VagaEstacionamentoMapper {
                 vagaEstacionamentoEntity.getColunaEstacionamento());
     }
 
-    public VagaEstacionamentoEntity fromDomain(VagaEstacionamento vagaEstacionamento){
+    public VagaEstacionamentoEntity fromDomain(VagaEstacionamento vagaEstacionamento) {
 
         return new VagaEstacionamentoEntity(
-            vagaEstacionamento.getLinhaEstacionamento(),
-            vagaEstacionamento.getColunaEstacionamento());
+                UUID.randomUUID().toString(),
+                vagaEstacionamento.getLinhaEstacionamento(),
+                vagaEstacionamento.getColunaEstacionamento());
     }
 
+    public VagaEstacionamentoEntity fromDomainComIdPredefinido(VagaEstacionamento vagaEstacionamento) {
+
+        return new VagaEstacionamentoEntity(
+                vagaEstacionamento.getId(),
+                vagaEstacionamento.getLinhaEstacionamento(),
+                vagaEstacionamento.getColunaEstacionamento());
+    }
 }
