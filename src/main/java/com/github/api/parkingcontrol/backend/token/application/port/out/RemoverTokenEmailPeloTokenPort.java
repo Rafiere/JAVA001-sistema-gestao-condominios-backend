b01234b1e46293e6315721bd170bf07f59dd10fe
Rafael <rafapgbr@gmail.com>
2022-08-07 09:37:15 -0300

    feat: inserção de porta para remover token pelo email.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/token/application/port/out/RemoverTokenEmailPeloTokenPort.java
@@ -0,0 +1,6 @@
+package com.github.api.parkingcontrol.backend.token.application.port.out;
+
+public interface RemoverTokenEmailPeloTokenPort {
+
+    void removerTokenEmailPeloToken(String token);
+}
