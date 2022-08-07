9ab037035f45be5f13fe453ee41d4f92d98c771a
Rafael <rafapgbr@gmail.com>
2022-08-07 09:32:37 -0300

    feat: inserção de command para ativar um usuário pelo token.

---
+++ src/main/java/com/github/api/parkingcontrol/backend/token/adapter/in/web/command/AtivarUsuarioPeloTokenCommand.java
@@ -0,0 +1,9 @@
+package com.github.api.parkingcontrol.backend.token.adapter.in.web.command;
+
+import lombok.Value;
+
+@Value
+public class AtivarUsuarioPeloTokenCommand {
+
+    String tokenDeConfirmacao;
+}
