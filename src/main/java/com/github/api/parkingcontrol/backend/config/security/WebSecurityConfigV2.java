1628bca04e15e910d947360e4760cbbdf2aa114d
Rafael <rafapgbr@gmail.com>
2022-08-07 09:31:52 -0300

    fix: correção da classes de configuração do Spring Security.

--- src/main/java/com/github/api/parkingcontrol/backend/config/security/WebSecurityConfigV2.java
+++ src/main/java/com/github/api/parkingcontrol/backend/config/security/WebSecurityConfigV2.java
@@ -1,6 +1,5 @@
 package com.github.api.parkingcontrol.backend.config.security;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,23 +13,30 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfigV2 {
 
+    final String URL_CADASTRO_USUARIO = "/v1/usuario/cadastrar-usuario";
+    final String URL_RECUPERACAO_DE_SENHA = "/v1/usuario/recuperar-senha";
+    final String URL_CONFIRMAR_EMAIL = "/email/confirmarEmail/**";
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        final String URL_RECUPERACAO_DE_SENHA = "NULL";
-
         http
+//                .httpBasic()
+//                .and()
+//                .csrf().disable()
+//                .authorizeRequests().anyRequest().permitAll();
                 .httpBasic()
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/usuario/cadastrar-usuario").hasRole("ADMINISTRADOR")
+                .antMatchers(URL_CADASTRO_USUARIO).hasAuthority("ROLE_ADMINISTRADOR")
+                .antMatchers(URL_RECUPERACAO_DE_SENHA).permitAll()
+                .antMatchers(URL_CONFIRMAR_EMAIL).permitAll()
                 .antMatchers("/**/swagger-ui/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/**/swagger-resources/**",
                         "/**/webjars/springfox-swagger-ui/**",
                         "/**/v2/api-docs/**").permitAll()
-                .anyRequest()
-                .authenticated();
+                .anyRequest().hasAnyAuthority("ROLE_ADMINISTRADOR", "ROLE_FUNCIONARIO");
 
         return http.build();
     }
